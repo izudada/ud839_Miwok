582e3d32aef3cbcd5bfbbe4ffce89fca9b81760a
Anthony <tonyudeagbala@gmail.com>
2022-07-01 03:01:59 +0100

    Feat: Create Word class

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,2 +1,25 @@
-package com.example.android.miwok;public class Word {
+package com.example.android.miwok;
+
+public class Word {
+
+    /**  Default word translation */
+    private String mDefaultTranslation;
+
+    /**  Miwok  translation for the word */
+    private String mMiwokTranslation;
+
+    public  Word(String defaultTranslation, String miwokTranslation) {
+        mMiwokTranslation = miwokTranslation;
+        mDefaultTranslation = defaultTranslation;
+    }
+
+    /** Get the default translation of the word */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /** Get the miwok translation of the word */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
 }
